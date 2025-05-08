76f62800f4c69393beb9a4acbe52d46f7a5a6eb9
GuyGabriell <jonahgabriel85@yahoo.com>
2025-05-08 19:28:27 +0100

    learn how to external libraries, DOM,external & internal script

---
+++ code-editor-&-executing-script/script.js
@@ -0,0 +1,7 @@
+/*you can also pull in external libraries from the net
+learn how to use both inline and external scripts
+*/
+
+console.log('External script');
+
+document.querySelector('p').style.color = 'green';
