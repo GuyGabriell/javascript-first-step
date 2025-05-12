1b7149250450cc525095b3d9f4f32f367c4fd8a1
GuyGabriell <jonahgabriel85@yahoo.com>
2025-05-12 12:27:04 +0100

    built in methods, oparating or functions

--- strings/script.js
+++ strings/script.js
@@ -1,5 +1,5 @@
 /* how to define o r declare a string 
-there are three cpmmon ways to declare a strings
+there are three common ways to declare a strings
 1. single quote
 2. double quote
 3. backtick
@@ -30,3 +30,41 @@ const aboutMe = 'name: ' + firstName + ' ' + lastName + ', job: ' + jobTitle;
 console.log(aboutMe);
 
 console.log(fullName);
+
+//now we use template strings
+//template strings are a new way to create strings
+
+const fullName2 = `name: ${firstName} ${lastName}, job: ${jobTitle}`;
+console.log(fullName2); //this is considered a more cleaner way to write the same thing
+
+const html = `
+<div>
+Hi there ${fullName}
+</div>
+`;
+console.log(firstName.length); 
+
+// built in methods, oparating or functions, they allow to manipulate strings.
+
+const html2 = `
+<div>
+Hi there ${fullName}
+</div>
+`;
+console.log(firstName.length); 
+//you can even grap some certain chars or part odf the str 
+console.log(firstName.charAt(0)); // returns the first char
+// you can also do that using the array notation
+console.log(firstName[0]); // returns the first char
+// how about indexOf
+console.log(firstName.indexOf('u')); // returns the index of the first occurence of the char u
+//using also the upper case
+console.log(firstName.toUpperCase()); // returns the string in upper case
+//using also the lower case
+console.log(firstName.toLowerCase()); // returns the string in lower case
+//part of the string using the substring method
+console.log(firstName.substring(0, 2)); // returns the first two chars
+//usig the includes method
+console.log(firstName.includes('uy')); // returns true or false
+
+
