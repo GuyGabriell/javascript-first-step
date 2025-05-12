b4c05cf411dd5658dffca6c341fffe24dd072562
GuyGabriell <jonahgabriel85@yahoo.com>
2025-05-12 13:17:22 +0100

    floating points inaccuracy & modulus operator

--- numbers/script.js
+++ numbers/script.js
@@ -14,4 +14,18 @@
 	console.log(num1 / num2); // returns 2
 
 
+	//floating points inaccuracy 
+	console.log(0.1 + 0.2); // returns 0.30000000000000004
+	console.log(0.2 + 0.4); // returns 0.6000000000000001
+
+	//other operations like modulus operator
+	//this is basically the remainder after you divide two numbers
+	console.log(13 % 5); // returns 3
+	console.log(4 % 2); // returns 0
+	// use the rise operator to raise a number to a power
+	console.log(2 ** 3); // returns 8
+	console.log(2 ** 4); // returns 16
+
+	// use the builtin math object
+	// Increment and Decrement
 	
