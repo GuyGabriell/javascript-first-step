f7493bb2bb2696872350e042d9fdc4d67069788f
GuyGabriell <jonahgabriel85@yahoo.com>
2025-05-12 13:04:20 +0100

    numbers and some of the basic operations

---
+++ numbers/script.js
@@ -0,0 +1,17 @@
+	//lets looks at numbers and some of the basic operations  you can perform with them.
+
+	//variables
+	const num1 = 4;
+	const num2 = 2;
+	const num3 = 4.5;
+	const num4 = 4.5;
+
+	//basic maths operations
+	//print out te result
+	console.log(num1 + num2); // returns 6
+	console.log(num1 - num2); // returns 2
+	console.log(num1 * num2); // returns 8
+	console.log(num1 / num2); // returns 2
+
+
+	
